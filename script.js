63870c26d6509c5d4fda3520fda96672bec14c99
Vasilisap <vapostolou99@gmail.com>
2024-11-03 23:44:29 +0200

    set random color and ten times opacity
    Each square get random color and oapcity is increased 10 times

--- script.js
+++ script.js
@@ -2,34 +2,61 @@ const container = document.querySelector(".grid-container");
 const btnSize = document.querySelector("#btn-size");
 
 const getUserInputSize = () => {
-	const input = prompt(
-		"Please select the number of squares for your grid\n1-100"
-	);
-	if ((isNaN(input) && !Number.isInteger(input)) || input < 1 || input > 100) {
-		console.log(typeof input);
+  const input = prompt(
+    "Please select the number of squares for your grid\n1-100"
+  );
+  if ((isNaN(input) && !Number.isInteger(input)) || input < 1 || input > 100) {
+    console.log(typeof input);
 
-		alert("Please choose between 1-100 numbers only");
-	} else {
-		setGridSize(input);
-	}
+    alert("Please choose between 1-100 numbers only");
+  } else {
+    setGridSize(input);
+  }
+};
+
+const setRandomColor = () => {
+  const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+  let color = "#";
+
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * hex.length)];
+  }
+
+  return color;
 };
 
 const setGridSize = (MAX_GRID_SIZE = 16) => {
-	container.innerHTML = ``;
-	for (let i = 0; i < MAX_GRID_SIZE; i++) {
-		container.insertAdjacentHTML("afterbegin", `<div class='grid-row'>`);
-		const gridRow = document.querySelector(".grid-row");
+  container.innerHTML = ``;
+  for (let i = 0; i < MAX_GRID_SIZE; i++) {
+    container.insertAdjacentHTML("afterbegin", `<div class='grid-row'>`);
+    const gridRow = document.querySelector(".grid-row");
 
-		for (let i = 0; i < MAX_GRID_SIZE; i++) {
-			gridRow.insertAdjacentHTML("afterbegin", `<div class='grid-square'>`);
-		}
-	}
+    for (let i = 0; i < MAX_GRID_SIZE; i++) {
+      gridRow.insertAdjacentHTML("afterbegin", `<div class='grid-square'>`);
+    }
+  }
 };
 
 container.addEventListener("mouseover", (e) => {
-	if (e.target === container) return;
-	const target = e.target;
-	if (!target.classList.contains("hover")) target.classList.add("hover");
+  if (e.target === container) return;
+
+  const target = e.target;
+  if (!target.dataset.color) {
+    const randomColor = setRandomColor();
+    target.style.backgroundColor = randomColor;
+    target.style.opacity = 0.1;
+    target.dataset.color = randomColor;
+  }
+
+  if (target.style.opacity < 1) {
+    if (target.dataset.timeshover && target.dataset.timeshover < 10) {
+      target.style.opacity = parseFloat(target.style.opacity) + 0.1;
+
+      target.dataset.timeshover++;
+    } else {
+      target.dataset.timeshover = 1;
+    }
+  }
 });
 
 btnSize.addEventListener("click", getUserInputSize);
